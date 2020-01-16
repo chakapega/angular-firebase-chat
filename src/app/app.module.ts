4f28e69bddf5500105950e715225e62466ec6470
Vadim <chakapega@gmail.com>
2020-01-17 02:28:37 +0300

    feat: message-box, message-send-form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MessageSendingFormComponent } from './message-sending-form/message-sending-form.component';
+import { MessageBoxComponent } from './message-box/message-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageSendingFormComponent,
+    MessageBoxComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/message-sending-form/message-sending-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-sending-form',
+  templateUrl: './message-sending-form.component.html',
+  styleUrls: ['./message-sending-form.component.scss']
+})
+export class MessageSendingFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
